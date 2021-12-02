f80de504b1ffc7b7bb1ad6a2c3f1ae4edda97156
Pabloleabr <pabolo333@gmail.com>
2021-12-02 18:47:17 +0100

    paginacion de vuelos

--- app/Http/Controllers/VuelosController.php
+++ app/Http/Controllers/VuelosController.php
@@ -14,8 +14,19 @@ class VuelosController extends Controller
         ->join('aeropuertos AS ae', 'destino_id', '=', 'ae.id')
         ->join('companias AS c', 'compania_id', '=', 'c.id')
         ->select('v.*', 'a.denominacion as origen', 'ae.denominacion as destino', 'c.denominacion as compania' )
-        ->get();
+        ;
 
-        return view('vuelos', ['vuelos' => $vuelos]);
+        $paginador = $vuelos->paginate(1);
+        /* $paginador->appends(compact(
+            'codigo',
+            'origen',
+            'destino',
+            'compania',
+            'salida',
+            'llegada',
+            'plazas',
+            'precio'
+        )); */
+        return view('vuelos', ['vuelos' => $paginador]);
     }
 }
